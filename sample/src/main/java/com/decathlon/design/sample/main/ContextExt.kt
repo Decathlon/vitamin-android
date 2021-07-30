63cecfa0f9d513287207f1bf634be731782b810f
Gerard Paligot <gerard.paligot@gmail.com>
2021-07-30 12:53:38 +0200

    feat: Add DarkMode style in foundation module.
    Closes #28

---
+++ sample/src/main/java/com/decathlon/design/sample/main/ContextExt.kt
@@ -0,0 +1,16 @@
+package com.decathlon.design.sample.main
+
+import android.content.Context
+import android.util.TypedValue
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
+
+@ColorInt
+fun Context.getColorFromAttr(
+    @AttrRes attrColor: Int,
+    typedValue: TypedValue = TypedValue(),
+    resolveRefs: Boolean = true
+): Int {
+    theme.resolveAttribute(attrColor, typedValue, resolveRefs)
+    return typedValue.data
+}
--- sample/src/main/java/com/decathlon/design/sample/main/MainFragment.kt
+++ sample/src/main/java/com/decathlon/design/sample/main/MainFragment.kt
@@ -1,6 +1,7 @@
 package com.decathlon.design.sample.main
 
 import android.content.res.Configuration
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
+import com.decathlon.design.sample.R
 import com.decathlon.design.sample.components.model.ComponentProvider
 import com.decathlon.design.sample.databinding.FragmentMainBinding
 import com.google.android.material.transition.MaterialSharedAxis
@@ -24,10 +26,8 @@ class MainFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -38,24 +38,29 @@ class MainFragment : Fragment() {
     }
 
     private fun setupRecyclerView() {
-        binding.recyclerView.apply {
-            val spanCount =
-                if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
-                    2
-                } else {
-                    4
+        this.context?.let { context ->
+            binding.recyclerView.apply {
+                val spanCount =
+                    if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+                        2
+                    } else {
+                        4
+                    }
+                layoutManager = GridLayoutManager(context, spanCount)
+                addItemDecoration(
+                    DividerItemDecoration(context, DividerItemDecoration.HORIZONTAL).apply {
+                        setDrawable(ColorDrawable(context.getColorFromAttr(R.attr.vtmnBorderPrimary)))
+                    }
+                )
+                addItemDecoration(
+                    DividerItemDecoration(context, DividerItemDecoration.VERTICAL).apply {
+                        setDrawable(ColorDrawable(context.getColorFromAttr(R.attr.vtmnBorderPrimary)))
+                    }
+                )
+                adapter = MainAdapter(ComponentProvider.getAll()) {
+                    findNavController().navigate(it)
                 }
-            layoutManager = GridLayoutManager(this.context, spanCount)
-            addItemDecoration(
-                DividerItemDecoration(this.context, DividerItemDecoration.HORIZONTAL)
-            )
-            addItemDecoration(
-                DividerItemDecoration(this.context, DividerItemDecoration.VERTICAL)
-            )
-            adapter = MainAdapter(ComponentProvider.getAll()) {
-                findNavController().navigate(it)
             }
         }
     }
-
 }
