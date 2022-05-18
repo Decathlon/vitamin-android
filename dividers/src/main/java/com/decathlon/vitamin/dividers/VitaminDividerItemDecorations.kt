22c8be3156609c3dbbc9b4139880f3e1c639a564
Gerard Paligot <gerard.paligot@gmail.com>
2022-05-18 17:27:56 +0200

    refactor(dividers): Apply new naming convention for ids.

--- dividers/src/main/java/com/decathlon/vitamin/dividers/VitaminDividerItemDecorations.kt
+++ dividers/src/main/java/com/decathlon/vitamin/dividers/VitaminDividerItemDecorations.kt
@@ -2,26 +2,25 @@ package com.decathlon.vitamin.dividers
 
 import android.content.Context
 import android.util.AttributeSet
-import com.google.android.material.divider.MaterialDivider
 import com.google.android.material.divider.MaterialDividerItemDecoration
 
 open class VitaminFullBleedDividerItemDecoration @JvmOverloads constructor(
     context: Context,
     orientation: Int,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.dividerFullBleed
+    defStyleAttr: Int = R.attr.vtmnDividerFullBleedStyle
 ) : MaterialDividerItemDecoration(context, attrs, defStyleAttr, orientation)
 
 open class VitaminInsetDividerItemDecoration @JvmOverloads constructor(
     context: Context,
     orientation: Int,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.dividerInset
+    defStyleAttr: Int = R.attr.vtmnDividerInsetStyle
 ) : MaterialDividerItemDecoration(context, attrs, defStyleAttr, orientation)
 
 open class VitaminMiddleDividerItemDecoration @JvmOverloads constructor(
     context: Context,
     orientation: Int,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.dividerMiddle
+    defStyleAttr: Int = R.attr.vtmnDividerMiddleStyle
 ) : MaterialDividerItemDecoration(context, attrs, defStyleAttr, orientation)
--- dividers/src/main/java/com/decathlon/vitamin/dividers/VitaminDividers.kt
+++ dividers/src/main/java/com/decathlon/vitamin/dividers/VitaminDividers.kt
@@ -7,17 +7,17 @@ import com.google.android.material.divider.MaterialDivider
 open class VitaminFullBleedDivider @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.dividerFullBleed
+    defStyleAttr: Int = R.attr.vtmnDividerFullBleedStyle
 ) : MaterialDivider(context, attrs, defStyleAttr)
 
 open class VitaminInsetDivider @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.dividerInset
+    defStyleAttr: Int = R.attr.vtmnDividerInsetStyle
 ) : MaterialDivider(context, attrs, defStyleAttr)
 
 open class VitaminMiddleDivider @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.dividerMiddle
+    defStyleAttr: Int = R.attr.vtmnDividerMiddleStyle
 ) : MaterialDivider(context, attrs, defStyleAttr)
