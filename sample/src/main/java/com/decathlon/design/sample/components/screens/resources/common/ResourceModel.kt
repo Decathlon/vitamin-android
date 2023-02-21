d25f739239b990245de04469d5796216747e1d57
ManonPolle <manon.polle@decathlon.com>
2023-02-21 15:44:55 +0100

    fix(icons): Fix semantic color for icons tint in sample app

--- sample/src/main/java/com/decathlon/design/sample/components/screens/resources/common/ResourceModel.kt
+++ sample/src/main/java/com/decathlon/design/sample/components/screens/resources/common/ResourceModel.kt
@@ -2,4 +2,9 @@ package com.decathlon.design.sample.components.screens.resources.common
 
 import android.graphics.drawable.Drawable
 
-data class ResourceModel(val name: String, val src: Drawable)
+data class ResourceModel(val name: String, val src: Drawable, val type: ResourceType)
+
+enum class ResourceType {
+    Icon,
+    Asset
+}
