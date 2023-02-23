c473a4c5a070322d9006c6588b74763975568471
ManonPolle <46710837+ManonPolle@users.noreply.github.com>
2023-02-23 17:25:47 +0100

    build: Add plugin to check conventional commit message format (#169)

--- buildSrc/src/main/kotlin/com/decathlon/vitamin/Versions.kt
+++ buildSrc/src/main/kotlin/com/decathlon/vitamin/Versions.kt
@@ -5,6 +5,7 @@ object Versions {
         const val minSdk = 21
         const val compileSdk = 30
     }
+
     const val kotlin = "1.5.31"
     const val material = "1.5.0-alpha01"
     const val appCompat = "1.2.0"
@@ -19,6 +20,7 @@ object Dependencies {
     const val appcompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
     const val constraint = "androidx.constraintlayout:constraintlayout:${Versions.constraint}"
     const val fragment = "androidx.fragment:fragment-ktx:${Versions.fragment}"
+
     object NavigationComponents {
         const val fragment = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
     }
